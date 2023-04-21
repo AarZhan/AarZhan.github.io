79584c70691e073612646cd72da1d2959fb6f879
AarZhan <zhang406@stcharles.k12.la.us>
2023-04-21 18:12:44 +0000

    circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -27,7 +27,7 @@ var init = function (window) {
         function drawCircle() {  
         // Code to draw a circle
 circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-physikz.addRandomVelocity(circle, canvas);
+physikz.addRandomVelocity(circle, canvas, 10 ,10);
 view.addChild(circle);
 circles.push(circle);
          }
@@ -69,20 +69,23 @@ for (var i = 0; i < circles.length; i++) {
         it to the opposite side of the screen.
         */
         game.checkCirclePosition = function(circle) {
-
+var rightEdge = circle.x + circle.radius;
+var leftEdge = circle.x - circle.radius;
+var bottomEdge = circle.y + circle.radius;
+var topEdge = circle.y - circle.radius;
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if ( leftEdge > canvas.width ) {
                 circle.x = 0;
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if ( circle.x < 0 ) {
+            if ( rightEdge < 0 ) {
                 circle.x = canvas.width;
             }
-            if ( circle.y > canvas.height ) {
+            if ( topEdge > canvas.height ) {
                 circle.y = 0;
             }
-            if ( circle.y < 0 ) {
+            if ( bottomEdge < 0 ) {
                 circle.y = canvas.height;
             }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
