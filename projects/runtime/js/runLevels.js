009422bc3fa630b5fdd886fb63717e560e230806
AarZhan <zhang406@stcharles.k12.la.us>
2023-05-18 18:00:53 +0000

    adding runtime

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -14,12 +14,85 @@ var runLevels = function (window) {
     var levelData = window.opspark.levelData;
 
     // set this to true or false depending on if you want to see hitzones
-    game.setDebugMode(true);
+    game.setDebugMode(false);
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
 
+    function createSawBlade(x){
+    var hitZoneSize = 25;
+    var damageFromObstacle = 10;
+    var sawBladeHitZone = game.createObstacle(hitZoneSize, 50);
+
+    sawBladeHitZone.x = 500 + i * 300;
+    sawBladeHitZone.y = 340;
+    game.addGameItem(sawBladeHitZone);
+
+    var obstacleImage = draw.bitmap("img/Mine.png");
+    sawBladeHitZone.addChild(obstacleImage);
+    obstacleImage.x = -50;
+    obstacleImage.y = -50;
+    obstacleImage.scaleX = .05;
+    obstacleImage.scaleY = .05;
+
     
+  }
+
+ 
+
+function createEnemy(x) {
+  var enemy = game.createGameItem("enemy", 150);
+  var tiger = draw.bitmap("img/tiger.png");
+  tiger.x = -250;
+  tiger.y = -160;
+  tiger.scaleX = 1.5;
+  tiger.scaleY = 1.5;
+  enemy.addChild(tiger);
+  enemy.x = x;
+enemy.y = 300;
+game.addGameItem(enemy);
+enemy.velocityX = -4;
+enemy.onPlayerCollision = function () {
+  game.changeIntegrity(-30)
+  }
+
+  enemy.onProjectileCollision = function () {
+    game.increaseScore(1000);
+    enemy.fadeOut();
+  }
+
+
+  function createReward(x) {
+    var reward = game.createGameItem("reward", 150);
+    var stick = draw.bitmap("img/tonk.png");
+    stick.x = -150;
+    stick.y = -70;
+    stick.scaleX = 1.5;
+    stick.scaleY = 1.5;
+    reward.addChild(stick);
+    reward.x = x;
+  reward.y = 300;
+  game.addGameItem(reward);
+  reward.velocityX = -4;
+  reward.onPlayerCollision = function () {
+    }
+  
+    reward.onProjectileCollision = function () {
+      game.increaseScore(10);
+      reward.fadeOut();
+    }}
+createReward(10000)
+}
+for (var i = 0; i < 50; ++i){
+  if (i % 2 !== 0)
+  console.log("a");
+  else
+  createEnemy(i * 1000 + 500);
+  createSawBlade(i +50 );
+}
+
+
+
 
     function startLevel() {
       // TODO 13 goes below here
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -24,13 +24,17 @@ var background = function (window) {
         
         // container which will be returned
         var background;
+        var mount;
         
         //////////////////////////////////////////////////////////////////
         // ANIMATION VARIABLES HERE //////////////////////////////////////
         //////////////////////////////////////////////////////////////////
         // TODO (several):
-      
-      
+      var cloud = 0;
+      var cloud1 = 0;
+      var cloud2 = 0;
+      var cloud3 = 0;
+      var stuka =0;
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -39,17 +43,55 @@ var background = function (window) {
             // TODO 1:
             // this currently fills the background with an obnoxious yellow;
             // you should modify both the height and color to suit your game
-            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'yellow');
+            var backgroundFill = draw.rect(canvasWidth,canvasHeight,'darkgreen');
             background.addChild(backgroundFill);
             
             // TODO 2: - Add a moon and starfield
-            
+            var ground = draw.rect(canvasWidth, 330, "lightblue");
+            background.addChild(ground);
+
+        
             
             // TODO 4: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
             
             
             // TODO 3: Part 1 - Add a tree
-            
+                cloud2 = draw.bitmap("img/cloud1.png");
+                cloud2.x = 700;
+                cloud2.y = -10;
+                cloud2.scaleX = .40;
+                cloud2.scaleY = .40;
+                background.addChild(cloud2);
+
+                cloud = draw.bitmap("img/cloud1.png");
+                cloud.x = 1000;
+                cloud.y = 10;
+                cloud.scaleX = .50;
+                cloud.scaleY = .50;
+                background.addChild(cloud);
+
+                cloud1 = draw.bitmap("img/cloud1.png");
+                cloud1.x = -200;
+                cloud1.y = 10;
+                cloud1.scaleX = .50;
+                cloud1.scaleY = .50;
+                background.addChild(cloud1);
+
+                cloud3 = draw.bitmap("img/cloud1.png");
+                cloud3.x = 300;
+                cloud3.y = 10;
+                cloud3.scaleX = .40;
+                cloud3.scaleY = .40;
+                background.addChild(cloud3);
+
+                stuka = draw.bitmap("img/Stuka.png");
+                stuka.x = 300;
+                stuka.y = 10;
+                stuka.scaleX = .20;
+                stuka.scaleY = .20;
+                background.addChild(stuka);
+                
+                
             
         } // end of render function - DO NOT DELETE
         
@@ -63,6 +105,27 @@ var background = function (window) {
             var groundY = ground.y;
             
             // TODO 3: Part 2 - Move the tree!
+            cloud.x = cloud.x - .5 ;
+            
+            if (cloud.x < -300) {
+            cloud.x = canvasWidth;}
+            cloud1.x = cloud1.x - .5 ;
+            
+            if (cloud1.x < -300) {
+            cloud1.x = canvasWidth;}
+
+            cloud2.x = cloud2.x - .3 ;
+            
+            if (cloud2.x < -300) {
+            cloud2.x = canvasWidth;}
+
+            if (cloud3.x < -300) {
+            cloud3.x = canvasWidth;}
+            cloud3.x = cloud3.x - .3 ;
+
+            if (stuka.x < -5000) {
+                stuka.x = canvasWidth;}
+                stuka.x = stuka.x - 10 ;
             
             
             // TODO 4: Part 2 - Parallax
